aa3c3db0acc383bf458db880efc1e7c44ae6d1c4
ALESSANDRODASILVALIMA <alessandrodasilva93@gmail.com>
2023-09-30 11:02:48 -0300

    Iniciando projeto RESTApi santander

---
+++ src/test/java/com/example/santanderdevapi/SantanderDevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santanderdevapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
