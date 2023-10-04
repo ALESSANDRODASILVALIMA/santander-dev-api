c4ed575661e65f652457a5165daa39bd7fedb8ab
ALESSANDRODASILVALIMA <alessandrodasilva93@gmail.com>
2023-10-03 21:28:26 -0300

    Hotfix cors swagger

--- src/main/java/com/example/santanderdevapi/Application.java
+++ src/main/java/com/example/santanderdevapi/Application.java
@@ -3,6 +3,10 @@ package com.example.santanderdevapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+//OpenAPIDefinition para nao da erro de cors no swagger
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
